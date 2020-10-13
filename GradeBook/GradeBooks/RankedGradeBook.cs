41bfce79976f72a760a89ee70205558ae789c0ae
Scored <multiusage2@o2.pl>
2020-10-13 16:41:46 +0200

    Day 1.1

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
