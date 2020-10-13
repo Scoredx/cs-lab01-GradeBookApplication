b4b9521ced00cb6dd6cb193e44f55fa854896689
Scored <multiusage2@o2.pl>
2020-10-13 18:49:04 +0200

    Day 1.2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,37 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double avgGrade)
+        {
+            int allStudents = Students.Count;
+
+            if (allStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int studentsWithHigherAverageGrade =
+                Students
+                .Select(a => a)
+                .Where(a => a.AverageGrade >= avgGrade)
+                .Count();
+            var percentOFStudentsWithHighAverage = Decimal.Divide(studentsWithHigherAverageGrade, allStudents) * 100;
+            var percent = (100 - percentOFStudentsWithHighAverage);
+
+            switch (percent)
+            {
+                case decimal percentage when (percent >= 80):
+                    return 'A';
+                case decimal percentage when (percent < 80 && percent >= 60):
+                    return 'B';
+                case decimal percentage when (percent < 60 && percent >= 40):
+                    return 'C';
+                case decimal percentage when (percent < 40 && percent >= 20):
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
     }
 }
