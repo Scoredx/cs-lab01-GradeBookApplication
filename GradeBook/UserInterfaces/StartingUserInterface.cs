92d3b0eee40365ba02f82093015d74fb7d578b8e
Scored <multiusage2@o2.pl>
2020-10-14 10:16:48 +0200

    Day 2.1

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,13 +34,30 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2];
+            var isWeighted = Convert.ToBoolean(parts[3]);
+
+            BaseGradeBook gradeBook;
+
+            switch (type)
+            {
+                case "standard":
+                    gradeBook = new StandardGradeBook(name, isWeighted);
+                    break;
+                case "ranked":
+                    gradeBook = new RankedGradeBook(name, isWeighted);
+                    break;
+                default:
+                    Console.WriteLine($"{type} is not a supported type of gradebook");
+                    return;
+            }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
